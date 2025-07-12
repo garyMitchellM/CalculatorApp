f332c2a71149c17389ca6ce8082f089b257e889e
Gary M Montero <117549519+garyMitchellM@users.noreply.github.com>
2025-07-12 13:03:29 -0400

    Changes to main class and main method

--- src/main/java/CalculatorMain.java
+++ src/main/java/CalculatorMain.java
@@ -1,6 +1,43 @@
 public class CalculatorMain {
+        static Calculator calc = new Calculator();
+
+    public static void addNums() {
+        for (int i = 0; i < 10000000; i++) {
+            calc.add(i, i + 1);
+        }
+    }
+
+    public static void subtractNums() {
+        for (int i = 10000000; i > 0 ; i--) {
+            calc.subtract(i, i - 1);
+        }
+    }
+
+    public static void multiplyNums() {
+        for (int i = 0; i < 10000000; i++) {
+            calc.multiply(i, i + 1);
+        }
+    }
+
+    public static void squareNums() {
+        for (int i = 0; i < 10000000; i++) {
+            calc.square(i);
+        }
+    }
+
+    public static void squareRootNums() {
+        for (int i = 0; i < 10000000; i++) {
+            calc.squareRoot(i);
+        }
+    }
+
     public static void main(String[] args){
-        Calculator calc = new Calculator();
-        System.out.println("3 + 4 = " + calc.add(3, 4));
+
+        addNums();
+        subtractNums();
+        multiplyNums();
+        squareNums();
+        squareRootNums();
+        System.out.println("Completed batch computation.");
     }
 }
